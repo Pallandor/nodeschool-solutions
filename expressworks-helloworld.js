b8518e5bf88a62adef8b6dbb62d18778dc4a8e22
Pallandor <roger.sejas@gmail.com>
2016-03-02 01:07:45 +1100

    Update solution with Express.js version

--- expressworks-helloworld.js
+++ expressworks-helloworld.js
@@ -1,10 +1,19 @@
-// Raw Node.js solution
-var http = require("http");
+// Express solution
+var express = require('express');
+var app = express();
 
-var rogerServer = http.createServer(function(req, res) {
-    if (req.method === 'GET' && req.url === '/home') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello World!');
-    }
+app.get('/home', function(req, res) {
+    res.end('Hello World!');
+    // self-query: Why can't I req.pipe(process.stdout)?
 }).listen(process.argv[2]);
 
+
+// // Raw Node.js solution
+// var http = require("http");
+
+// var rogerServer = http.createServer(function(req, res) {
+//     if (req.method === 'GET' && req.url === '/home') {
+//         res.writeHead(200, { 'Content-Type': 'text/plain' });
+//         res.end('Hello World!');
+//     }
+// }).listen(process.argv[2]);
