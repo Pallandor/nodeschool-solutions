0bbdbf4d46c0c33cd2f1f0276d2a903950a4f046
Pallandor <roger.sejas@gmail.com>
2016-03-07 13:08:57 +1100

    Added extra comments

--- expressworks-whatsinquery.js
+++ expressworks-whatsinquery.js
@@ -1,13 +1,4 @@
-// //  Master Express.js and have fun!
-// // ─────────────────────────────────
-// //  WHAT'S IN QUERY
-// //  Exercise 7 of 8
-
-// // Oftentimes, we need to process the data from the URL query string (urlencoded).
-
-// // Write a route that extracts data from the query string in the GET /search URL
-// // route, e.g. ?results=recent&include_tabs=true and then outputs it back to
-// // the user in JSON format.
+// Express solution
 
 var express = require('express');
 var bodyparser = require('body-parser');
@@ -15,28 +6,8 @@ var querystring = require('querystring');
 
 var app = express();
 
-// app.use('/search/:QUERYSTR', function(req, res, next){
-// 	console.log(req.params.QUERYSTR); 
-// 	console.log('in other middleware..'); 
-// 	next(); 
-// });
-
-// strict pathing. no var name in mountpath for app.get ?? 
-
-// TWO TECH: 1, var name in route path.
-// 2: just use good url parser. 
-// 3: regexp
-// 4: expected no, of chars slice. 
-
-// app.use(bodyparser.urlencoded({extended:true})); // func returns func? 
-
 app.get('/search', function(req, res) {
 	res.end(JSON.stringify(req.query)); 
+	// or can use Express' res.send method for auto JSON stringification
+	// res.send(req.query); 
 }).listen(process.argv[2]);
-
-// // Use app.get('/search', function(){...}) for the route.
-
-// // In Express.js, to extract query string parameters, we can use (inside the 
-// request handler):
-
-// //     req.query.NAME
