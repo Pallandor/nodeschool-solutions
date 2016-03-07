433af8d80302ab80a17fb48be9041d550facc269
Pallandor <roger.sejas@gmail.com>
2016-03-07 21:58:56 +1100

    Express Streaming solution with understanding of testing for JSON

--- expressworks-jsonme.js
+++ expressworks-jsonme.js
@@ -1,70 +1,27 @@
-//  Master Express.js and have fun!
-// ─────────────────────────────────
-//  JSON ME
-//  Exercise 8 of 8
-
-// Most of the times we're building RESTful API servers with JSON.
-
-// Write a server that, when it receives a GET, reads a file, parses it to JSON,
-// and responds with that content to the user.
-
-// The server should respond to any GET that matches the /books resource path.
-// As always, the port is passed in process.argv[2]. The file to read is passed
-// in process.argv[3].
-
 // Express solution
-// // Streaming version 
+// // Streaming version / ES6
 var express = require('express');
 var fs = require('fs');
 var through = require('through2');
-var concat = require('concat-stream'); 
+var concat = require('concat-stream');
 
 var app = express();
 var port = process.argv[2];
 var fileToRead = process.argv[3];
 
-// SOLUTION V3 (async entire file read... don't know why concatstream isnt
-// equivalent to this though. Needs further investigation.)
-
-
-
-// SOLUTION V2
-// They're expecting a single JSON string chunk. So use concat-stream 
-// Although apparently they just do it synchronously reading file lol.. 
-// app.get('/books', function(req, res) {
-//     fs.createReadStream(fileToRead)
-//         .pipe(concat(function(finalBuffer){
-//         	// console.log(typeof finalBuffer); // assume its obj.. 
-//         	res.end(finalBuffer.toString()); 
-//         	// res.send(finalBuffer.toString()); 
-//         }));
-// }).listen(port);
-
-// // SOLUTION V1
-// // Unsure if I'm supposed to be passing pure objects i.e. non-string.
-// // Would need to enable objectMode options on all streams. 
-// app.get('/books', function(req, res) {
-//     fs.createReadStream(fileToRead)
-//         .pipe(through(function(chunk, _, next) {
-//             this.push(chunk.toString()); // Try using the encoding option instead. 
-//             next();
-//         }))
-//         .pipe(res);
-// }).listen(port);
-
-
-
-
-
-
-
-
-// Respond with:
-
-//     res.json(object)
-
-// Everything should match the /books resource path.
-
-// For reading the file, use the fs module, e.g.,
-
-//     fs.readFile(filename, callback)
+// I understand their solution. I was assuming a valid JSON string
+// was being read from file. But I can't assume. And the only way
+// to test if it's valid is to JSON.parse the buffer/string being read
+// and catch any errors. If there's no error, then onforward it again
+// as stringified JSON. 
+app.get('/books', (req, res) => {
+    fs.createReadStream(fileToRead)
+        .pipe(concat(finalBuffer => {
+            try {
+                var books = JSON.parse(finalBuffer.toString());
+            } catch (err) {
+                res.sendStatus(500);
+            }
+            res.send(JSON.stringify(books)); // They used res.json(obj); 
+        })); 
+}).listen(port);
