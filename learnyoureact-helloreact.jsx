b568d588b2651663f6cdde37911b607c84f88c15
Pallandor <roger.sejas@gmail.com>
2016-03-31 08:35:52 +1100

    Simple component rendering

---
+++ learnyoureact-helloreact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Test extends React.Component {
+
+  render(){
+    return (
+      <div className='todoBox'>Hello, world!</div>
+      )
+  }
+
+}
+
+export default Test; 
