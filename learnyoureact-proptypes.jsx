2eb8d8fb6b0bc4fcbefb76552c0730e81c892dda
Pallandor <roger.sejas@gmail.com>
2016-03-31 12:47:49 +1100

    Prop data validation

---
+++ learnyoureact-proptypes.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+class Test extends React.Component {
+
+  render(){
+    return (
+      <div className='todoBox'>
+      	<h1>Todos</h1>
+      	<TodoList />
+      	<TodoForm />
+      </div>
+      )
+  }
+
+}
+
+class TodoList extends React.Component {
+	render(){
+		return (
+			<div className='todoList'>
+				<table style={{border:'2px solid black'}}>
+					<tbody>
+						<Todo title='Shopping'>Milk</Todo>
+						<Todo title='Hair cut'>13:00</Todo>
+						<Todo title='Learn React'>15:00</Todo>
+					</tbody>
+				</table>
+			</div>
+		)
+	}
+}
+
+class Todo extends React.Component {
+	render(){
+		return(
+			<tr>
+				<td style={{border:'1px solid black'}}>
+					{this.props.title}
+				</td>
+				<td style={{border:'1px solid black'}}>
+					{this.props.children}
+				</td>
+			</tr>
+		)
+	}
+}
+
+// so it's a prop that takes an object with validators..
+Todo.propTypes = {
+	title: React.PropTypes.string.isRequired,
+	children: React.PropTypes.string.isRequired
+};
+
+var TodoForm = React.createClass({
+	render (){
+		return  <div className='todoForm'>I am a TodoForm.</div>
+	}
+});
+
+
+export default Test; 
