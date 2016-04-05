da9fc0a317e0b0444a1caad77f39573d3703c15e
Pallandor <roger.sejas@gmail.com>
2016-04-05 13:31:35 +1000

    Completed React Server and FE side rendering

---
+++ program.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var app = express();
+
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+var DOM = React.DOM;
+var body = DOM.body;
+var div = DOM.div;
+var script = DOM.script;
+
+var browserify = require('browserify');
+var babelify = require("babelify");
+
+
+// can port setting key str be anything e.g. 'p'? 
+app.set('p', (process.argv[2] || 3000));
+
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+// why is this part needed? 
+app.engine('jsx', require('express-react-views').createEngine({ transformViews: false }));
+
+// I assume for the es6 goodness? 
+require('babel/register')({
+    ignore: false
+});
+
+var TodoBox = require('./views/index.jsx');
+
+var data = [{
+    title: 'Shopping',
+    detail: process.argv[3]
+}, {
+    title: 'Hair cut',
+    detail: process.argv[4]
+}];
+
+app.use('/bundle.js', function(req, res) {
+    res.setHeader('content-type', 'application/javascript');
+
+    browserify({ debug: true })
+        .transform(babelify.configure({
+            presets: ["react", "es2015"]
+        }))
+        .require("./app.js", { entry: true })
+        .bundle()
+        .pipe(res);
+});
+
+
+app.use('/', function(req, res) {
+    var initialData = JSON.stringify(data);
+    var markup = ReactDOMServer.renderToString(React.createElement(TodoBox, { data: data }));
+    res.setHeader('Content-Type', 'text/html');
+
+    var html = ReactDOMServer.renderToStaticMarkup(body(null,
+        div({ id: 'app', dangerouslySetInnerHTML: { __html: markup } }),
+        script({
+            id: 'initial-data',
+            type: 'text/plain',
+            'data-json': initialData
+        }),
+        script({ src: '/bundle.js' })
+    ));
+
+    res.end(html);
+
+});
+
+
+
+app.listen(app.get('p'), function() {});
---
+++ views/index.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+
+export default class TodoBox extends React.Component {
+
+  render(){
+    return (
+      <div className='todoBox'>
+      	<h1>Todos</h1>
+      	<TodoList data={this.props.data}/>
+      	<TodoForm />
+      </div>
+      )
+  }
+}
+
+class TodoList extends React.Component {
+	render(){
+	
+		var todo = this.props.data.map(function(obj, i){
+			return <Todo key={i} title={obj.title} key={obj.title}>{obj.detail}</Todo>
+		}); 
+
+		return (
+			<div className='todoList'>
+				<table style={style.tableContent}>
+					<tbody>
+						{todo}
+					</tbody>
+				</table>
+			</div>
+		)
+	}
+}
+
+//class Todo extends React.Component ({
+
+	var Todo = React.createClass({
+		getInitialState(){
+			return {checked: false}; 
+		},
+
+		handleChange(e){
+			this.setState({checked:e.target.checked});
+		},
+
+		render(){
+		return(
+			<tr>
+				<td style={style.cellContent}>
+					<input type='checkbox' checked={this.state.checked} onChange={this.handleChange}/>
+				</td>
+				<td style={style.cellContent}>
+					{this.props.title}
+				</td>
+				<td style={style.cellContent}>
+					{this.props.children}
+				</td>
+			</tr>
+		)
+	}
+})
+
+//so it's a prop that takes an object with validators..
+Todo.propTypes = {
+	title: React.PropTypes.string.isRequired,
+	//children: React.PropTypes.string.isRequired
+};
+
+var TodoForm = React.createClass({
+	render (){
+		return  <div className='todoForm'>I am a TodoForm.</div>
+	}
+});
+
+let style = {
+	tableContent: {
+		border: '2px solid black'
+	},
+	cellContent: {
+		border: '1px solid black'
+	}
+}; 
+
+
+// export default Test; 
